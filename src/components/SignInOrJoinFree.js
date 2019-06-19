f5d10b77868a6cbaa43e0ee7750a8ea752d82433
Xavier Damman <xdamman@gmail.com>
2019-06-19 13:06:30 +0200

    fix(signin): default to create account

--- src/components/SignInOrJoinFree.js
+++ src/components/SignInOrJoinFree.js
@@ -40,7 +40,7 @@ class SignInOrJoinFree extends React.Component {
   };
 
   state = {
-    form: 'signin',
+    form: 'create-account',
     error: null,
     submitting: false,
     unknownEmailError: false,
